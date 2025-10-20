bb9b6128d16aee63976a4b3a7750afc5b034d9ba
Takumi <jiangqianye307@gmail.com>
2025-10-20 13:11:13 +0900

    攻撃実装前

--- style/js/enemy.js
+++ style/js/enemy.js
@@ -11,18 +11,19 @@ function keydown(){
 })
 }
 
+//撃の実装
 function enemy_attack(){
     // 球の形
     const attack = document.createElement("img");
     attack.src = "../assets/images/enemy_attack.png";
-    attack.className = "enemy_attack";
+    attack.className = "enemy_bullet";
     attack.style.position = "absolute";
 
     //敵の位置を取得
     const enemy_pos = enemy.getBoundingClientRect();
 
     //球の位置
-    attack.style.left = `${enemy_pos.left + enemy_pos.width/2 - attack.width/2}px`;
+    attack.style.left = `${enemy_pos.left + enemy_pos.width/2 - 50}px`;
     attack.style.top = `${enemy_pos.bottom}px`;
 
     document.body.appendChild(attack);
@@ -31,6 +32,7 @@ function enemy_attack(){
 
 
 }
+
 keydown();
 
 // const fireInterval = 2000;
