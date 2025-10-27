64644303f5acb19a1dfe0d29c83bf910a4f36002
TK250322 <tk250322@t.iput.ac.jp>
2025-10-27 23:17:37 +0900

    タイトルのスタートボタンを脱出編、UFO編、BOSS編の3つ構成に修正

--- str/title.js
+++ str/title.js
@@ -8,21 +8,44 @@ bgm.loop = true;
 bgm.volume = 0.5;
 
 // ページ読み込み後に再生
-window.addEventListener('load', () => {
+window.addEventListener('pageshow', () => {
   bgm.play().catch(err => {
   });
 });
 
+const debris_button = document.getElementById('debris_button');
+const UFO_button = document.getElementById('UFO_button');
+const boss_button = document.getElementById('save_button');
 // ボタンを取得
-const button = document.getElementById('save_button');
-
-button.addEventListener('click', () => {
+// debris ボタン
+debris_button.addEventListener('click', () => {
   const decisionSound = new Audio('../assets/sounds/effects/button.mp3');
   decisionSound.volume = 1.0;
   decisionSound.currentTime = 0;
+  decisionSound.play().catch(() => {});
+  setTimeout(() => {
+    window.location.href = 'debris.html';
+  }, 700);
+});
 
-decisionSound.play().catch(() => {});
-setTimeout(() => {
-  window.location.href = 'game_area.html';
-}, 700);
+// UFO ボタン
+UFO_button.addEventListener('click', () => {
+  const decisionSound = new Audio('../assets/sounds/effects/button.mp3');
+  decisionSound.volume = 1.0;
+  decisionSound.currentTime = 0;
+  decisionSound.play().catch(() => {});
+  setTimeout(() => {
+    window.location.href = 'UFO.html';
+  }, 700);
+});
+
+// boss（スタート）ボタン
+boss_button.addEventListener('click', () => {
+  const decisionSound = new Audio('../assets/sounds/effects/button.mp3');
+  decisionSound.volume = 1.0;
+  decisionSound.currentTime = 0;
+  decisionSound.play().catch(() => {});
+  setTimeout(() => {
+    window.location.href = 'game_area.html';
+  }, 700);
 });
