df10ab2401bb08801cebae6a4fc8a5348e2f08ac
千葉匠 <chibatakumi@chibatakuminoMacBook-Air.local>
2025-11-20 00:15:19 +0900

    デブリ爆発

--- str/finish.js
+++ str/finish.js
@@ -1,39 +1,39 @@
 window.go_result = function(){
     let clearStatus = "default_clear"; // デフォルトの目印
 
-    if(typeof ufo != "undefined"){
-        sessionStorage.setItem("ufo_time", seconds);
+    if(typeof ufo != "undefined"){
+        sessionStorage.setItem("ufo_time", seconds);
         clearStatus = "ufo_clear"; // ★UFOクリアの目印
-    }
+    }
 
-     else if (typeof debris != "undefined") {
-         clearStatus = "debris_clear"; // ★デブリクリアの目印
-     }
-    
+    else if (typeof debris != "undefined") {
+        clearStatus = "debris_clear"; // ★デブリクリアの目印
+    }
+
     // ボスステージの判定
-    if(typeof ufo == "undefined" && typeof debris == "undefined"){
-        console.log("勝ったんだ？すごいじゃん！！");
-        sessionStorage.setItem("boss_time", seconds);
+    if(typeof ufo == "undefined" && typeof debris == "undefined"){
+        console.log("勝ったんだ？すごいじゃん！！");
+        sessionStorage.setItem("boss_time", seconds);
         clearStatus = "boss_clear"; // ★ボスクリアの目印
-    }
+    }
 
-    move = false;
-    const screen = document.getElementById("win-screen");
-    screen.style.display = "block";
-    screen.addEventListener("click",()=>{
+    move = false;
+    const screen = document.getElementById("win-screen");
+    screen.style.display = "block";
+    screen.addEventListener("click",()=>{
         // ★★★ ここのURLを修正 ★★★
         // 目印（clearStatus）を付けて result.html に遷移
         console.log("crear");
-        window.location.href = `result.html?status=${clearStatus}`;
-    })
+        window.location.href = `result.html?status=${clearStatus}`;
+    });
 }
 
 window.gameover = function(){
-    console.log("負けたねー😊");
-    move = false;
-    const screen = document.getElementById("gameover-screen");
-    screen.style.display = "block";
-    screen.addEventListener("click",()=>{
-        window.location.href = "Title.html";
-    });
+    console.log("負けたねー😊");
+    move = false;
+    const screen = document.getElementById("gameover-screen");
+    screen.style.display = "block";
+    screen.addEventListener("click",()=>{
+        window.location.href = "Title.html";
+    });
 }
